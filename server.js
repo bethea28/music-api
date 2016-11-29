37ec3e0342545d84167ad08b1298429b1c6ac4e0
Samantha Bretous <sam@samanthabretous.com>
2016-11-29 16:07:12 -0500

    configure router for /api/artist

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //listen on port 8888
 app.listen('8888', () => console.log('Listening on port 8888'));
 
+const models = require('./index').models
 const routes = require('./index').routes
 
 app.use("/api/songs", routes.songs)
-app.use("/api/artist", routes.songs)
+app.use("/api/artists", routes.artists)
 
 //serve all of the HTML views, which we'll eventually use to build a simple user interface
 app.use("/view", routes.view)
